a1b2fc11b5f0d215e661d8cbc41b82d9a045ccdc
N_DK <nguyenkhoadang135759@gmail.com>
2024-05-12 00:21:32 +0700

    add loading

--- app/components/Loading.js
+++ app/components/Loading.js
@@ -4,8 +4,11 @@ import { HEIGH, WIDTH } from "../constants";
 
 function Loading() {
   return (
-    <View style={{width: WIDTH, height: HEIGH}} className="absolute flex items-center justify-center">
-      <Progress.CircleSnail thickness={10} size={120} color='rgb(15, 23, 42)'/>
+    <View
+      style={{ width: WIDTH, height: HEIGH }}
+      className="absolute flex items-center justify-center"
+    >
+      <Progress.CircleSnail thickness={10} size={120} color="rgb(15, 23, 42)" />
     </View>
   );
 }
